1a94fda39f259572cbec21a5bc7f7eabf26d901c
Tom Pohl <thomas.pohl@coherentminds.de>
2025-01-05 11:22:48 +0100

    docs: fix turn off comment

--- Adafruit_ESP32_Arduino_Demos/ESP32_sleeptest/ESP32_sleeptest.ino
+++ Adafruit_ESP32_Arduino_Demos/ESP32_sleeptest/ESP32_sleeptest.ino
@@ -70,7 +70,7 @@ void enableInternalPower() {
 #endif
 
 #if defined(ARDUINO_ADAFRUIT_FEATHER_ESP32S2)
-  // turn on the I2C power by setting pin to opposite of 'rest state'
+  // turn off the I2C power by setting pin to opposite of 'rest state'
   pinMode(PIN_I2C_POWER, INPUT);
   delay(1);
   bool polarity = digitalRead(PIN_I2C_POWER);
